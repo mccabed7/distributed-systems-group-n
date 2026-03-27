50374ae7006601e5530a11abd9436a9a1d498429
Sam <sktylr@gmail.com>
2026-03-27 23:35:45 +0000

    notification-srv: abstract websocket connection for improved backpressure performance
    The original implementation was blocking, meaning when handling redis messages,
    we blocked waiting to send the message to the client. If the client is slow, or
    the rate of message delivery is high, we will end up under significant backpressure.
    This change abstracts the message sending to use an event-driven queue (since the
    sending is largely I/O bound), which should help to relieve some of these issues.
    We also add a maximum number (per replica pod) of connections per user to keep the
    per-user message delivery loop shorter.

--- services/notification-srv/websocket.py
+++ services/notification-srv/websocket.py
@@ -10,11 +10,14 @@ import clients
 
 logger = get_logger("websockets")
 
+MAX_CONNECTIONS_PER_USER = 10
+MAX_QUEUE_CAPACITY = 100
+
 
 class WebSocketConnectionPool:
     def __init__(self, redis: clients.RedisClient):
         self._redis = redis
-        self._connections: Dict[str, Set[websockets.ServerConnection]] = defaultdict(set)
+        self._connections: Dict[str, Set[Connection]] = defaultdict(set)
         self._connections_lock = asyncio.Lock()
 
     async def send(self, user_id: str, message: str) -> None:
@@ -26,6 +29,10 @@ class WebSocketConnectionPool:
 
         dead = []
         for conn in conns:
+            if conn.is_closed():
+                dead.append(conn)
+                continue
+
             try:
                 await conn.send(message)
             except (Exception,) as e:
@@ -38,6 +45,12 @@ class WebSocketConnectionPool:
         async with self._connections_lock:
             for conn in dead:
                 self._connections[user_id].discard(conn)
+                if not self._connections[user_id]:
+                    del self._connections[user_id]
+                    await self._redis.deregister_user(user_id)
+
+        for conn in dead:
+            await conn.close()
 
     async def serve(self) -> None:
         await websockets.serve(self._handler, "0.0.0.0", 8080)
@@ -49,9 +62,15 @@ class WebSocketConnectionPool:
             await ws.close()
             return
 
-        async with self._connections_lock:
-            self._connections[user_id].add(ws)
         await self._redis.register_user(user_id)
+        connection = Connection(ws)
+        async with self._connections_lock:
+            user_connections = self._connections[user_id]
+            if len(user_connections) >= MAX_CONNECTIONS_PER_USER:
+                await ws.close()
+                return
+
+            self._connections[user_id].add(connection)
 
         try:
             while True:
@@ -62,7 +81,56 @@ class WebSocketConnectionPool:
             async with self._connections_lock:
                 if user_id not in self._connections:
                     return
-                self._connections[user_id].discard(ws)
+                self._connections[user_id].discard(connection)
                 if not self._connections[user_id]:
                     del self._connections[user_id]
                     await self._redis.deregister_user(user_id)
+
+
+class Connection:
+    def __init__(self, ws: websockets.ServerConnection):
+        self._ws = ws
+        self._queue = asyncio.Queue(maxsize=MAX_QUEUE_CAPACITY)
+        self._closed = False
+        self._sender_task = asyncio.create_task(self._sender())
+
+    async def send(self, message: str) -> None:
+        if self._closed:
+            raise RuntimeError("Connection closed")
+
+        try:
+            self._queue.put_nowait(message)
+        except asyncio.QueueFull:
+            raise RuntimeError("Client too slow")
+
+    def is_closed(self) -> bool:
+        return self._closed
+
+    async def close(self):
+        if self._closed:
+            return
+
+        self._closed = True
+        self._sender_task.cancel()
+
+        try:
+            await self._sender_task
+        except asyncio.CancelledError:
+            pass
+
+        await self._ws.close()
+
+    async def _sender(self):
+        try:
+            while True:
+                message = await self._queue.get()
+                try:
+                    await self._ws.send(message)
+                except Exception as e:
+                    logger.error("Failed to send message, err=%s", e)
+                    break
+        except asyncio.CancelledError:
+            # This might happen during shutdown but is fine
+            pass
+        finally:
+            self._closed = True
