30fd07b4f2971275a58582ec6940e78a6d2f379b
Sam <sktylr@gmail.com>
2026-04-04 18:14:52 +0100

    ui: add form for booking creation
    Verified that this reached the correct service, however it obviously
    fails once it reached gateway-srv since user-srv is unavailable and
    the request cannot be authenticated (obviously booking-srv is also
    unavailable if the request gets through this anyway).

--- ui/src/pages/CreateBookingPage.tsx
+++ ui/src/pages/CreateBookingPage.tsx
@@ -1,3 +1,57 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useBookingsApi } from "../api/bookings";
+import type { Location } from "../types"
+
 export default function CreateBookingPage() {
-	return <div>Create Booking Page</div>;
+  const [request, setRequest] = useState<BookingRequest>({});
+  const [loading, setLoading] = useState(false);
+  const [idempotencyKey] = useState(() => crypto.randomUUID());
+
+  const { createBooking } = useBookingsApi();
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    if (loading) {
+	  return;
+    }
+
+    setLoading(true);
+
+    try {
+      await createBooking(request, idempotencyKey);
+      navigate("/bookings");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h2>Create Booking</h2>
+
+      <input
+        placeholder="Origin"
+        value={request.origin}
+        onChange={(e) => setRequest(previous => ({ ...previous, origin: e.target.value}))}
+      />
+
+      <input
+        placeholder="Destination"
+        value={request.destination}
+        onChange={(e) => setRequest(previous => ({ ...previous, destination: e.target.value}))}
+      />
+
+      <input
+        type="date"
+        value={request.date}
+        onChange={(e) => setRequest(previous => ({ ...previous, date: e.target.value}))}
+      />
+
+      <button type="submit" disabled={loading}>
+        {loading ? "Creating..." : "Create Booking"}
+      </button>
+    </form>
+  );
 }
--- ui/src/types/index.ts
+++ ui/src/types/index.ts
@@ -6,3 +6,18 @@ export interface User {
 	role: Role;
 	token: string;
 }
+
+export type Location = string;
+
+export type BookingStatus = "successful" | "cancelled";
+
+export interface BookingRequest {
+  origin: Location;
+  destination: Location;
+  date: string;
+}
+
+export interface Booking extends BookingRequest {
+  id: string;
+  status: BookingStatus;
+};
