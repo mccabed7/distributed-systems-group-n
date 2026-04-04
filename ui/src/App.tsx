9e9a44e5135b393e4c38c2625bff80fff8f95b69
Sam <sktylr@gmail.com>
2026-04-04 16:13:19 +0100

    ui: add mock user authentication
    Currently there is no contacting of the API etc.
    Also, the admin ability is selectable on the client, which is
    fine for the demonstration purposes.

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -3,14 +3,36 @@ import LoginPage from "./pages/LoginPage";
 import BookingsPage from "./pages/BookingsPage";
 import CreateBookingPage from "./pages/CreateBookingPage";
 import AdminPage from "./pages/AdminPage";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 export default function App() {
   return (
     <Routes>
       <Route path="/login" element={<LoginPage />} />
-      <Route path="/bookings" element={<BookingsPage />} />
-      <Route path="/bookings/new" element={<CreateBookingPage />} />
-      <Route path="/admin" element={<AdminPage />} />
+      <Route
+        path="/bookings"
+        element={
+          <ProtectedRoute>
+              <BookingsPage />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/bookings/new"
+        element={
+          <ProtectedRoute>
+              <CreateBookingPage />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/admin"
+        element={
+          <ProtectedRoute requireAdmin>
+              <AdminPage />
+          </ProtectedRoute>
+        }
+      />
       <Route path="*" element={<Navigate to="/login" />} />
     </Routes>
   );
